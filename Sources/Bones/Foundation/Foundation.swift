8024a83eccbf8522d6af56ec32725304ff11b45b
Quentin Yann PIDOUX <quentin.pidoux@gmail.com>
2024-03-13 11:42:57 +0100

    Refactor Bones class with new color and spacing override methods
    - Added a new constructor to allow for setting a global color and spacing scheme at the start of the application
    - Added `overrideColor` method to override a specific color token with a new hex value
    - Updated `overrideSpacings` method to support real-time adjustments to the application's spacing scheme
    - Added `overrideSpacing` method to override a specific spacing token with a new value

--- Sources/Bones/Foundation/Foundation.swift
+++ Sources/Bones/Foundation/Foundation.swift
@@ -11,7 +11,7 @@ import SwiftUI
 public final class Bones {
     /// Initializes the application's theme with custom colors and spacing.
     ///
-    /// This method allows for the setting of a global color and spacing scheme at the start
+    /// This constructor allows for the setting of a global color and spacing scheme at the start
     /// of the application. It can be called during the application's initialization phase
     /// to ensure that the custom themes are applied throughout the app.
     ///
@@ -37,14 +37,38 @@ public final class Bones {
     bonesColors = colors
   }
   
+    /// Overrides a specific color token with a new hex value.
+    ///
+    /// This allows for fine-grained control over the color scheme, enabling dynamic changes
+    /// to individual colors.
+    ///
+    /// - Parameters:
+    ///   - color: The `Color.BonesColorToken` to override.
+    ///   - hexValue: The new hex color string.
+  public class func overrideColor(_ color: Color.BonesColorToken, with hexValue: String) {
+    bonesColors[color] = hexValue
+  }
+  
     /// Overrides the current spacing values with new values.
     ///
     /// Similar to `overrideColors`, this method enables dynamic updates to the application's
     /// spacing scheme. It supports real-time adjustments to layout and spacing based on
     /// user interactions or preferences.
     ///
-    /// - Parameter spacing: A dictionary mapping `CGFloat.BonesSpacing` to spacing values.
-  public class func overrideSpacings(_ spacing: [CGFloat.BonesSpacing: CGFloat]) {
-    bonesSpacings = spacing
+    /// - Parameter spacings: A dictionary mapping `CGFloat.BonesSpacing` to spacing values.
+  public class func overrideSpacings(_ spacings: [CGFloat.BonesSpacing: CGFloat]) {
+    bonesSpacings = spacings
+  }
+  
+    /// Overrides a specific spacing token with a new value.
+    ///
+    /// This allows for precise adjustments to the layout spacing, facilitating real-time
+    /// updates and customizations.
+    ///
+    /// - Parameters:
+    ///   - spacing: The `CGFloat.BonesSpacing` to override.
+    ///   - value: The new spacing value as a `CGFloat`.
+  public class func overrideSpacing(_ spacing: CGFloat.BonesSpacing, with value: CGFloat) {
+    bonesSpacings[spacing] = value
   }
 }
